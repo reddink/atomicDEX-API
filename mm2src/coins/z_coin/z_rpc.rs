15f1556c80bf0ecd1e4c260784f1c65ed3104e38
Artem Vitae <artem@vitae.com>
2022-04-27 17:03:49 +0700

    ZcoinLightClient WIP.

--- mm2src/coins/z_coin/z_rpc.rs
+++ mm2src/coins/z_coin/z_rpc.rs
@@ -4,16 +4,23 @@ use common::async_blocking;
 use common::jsonrpc_client::{JsonRpcClient, JsonRpcRequest};
 use common::mm_error::prelude::*;
 use common::mm_number::MmNumber;
+use db_common::sqlite::rusqlite::{params, Connection, Error};
 use http::Uri;
+use prost::Message;
+use protobuf::Message as ProtobufMessage;
 use rpc::v1::types::{Bytes as BytesJson, H256 as H256Json, H264 as H264Json};
 use rustls::ClientConfig;
 use serde_json::{self as json, Value as Json};
 use std::path::Path;
 use tonic::transport::{Channel, ClientTlsConfig};
-use zcash_client_backend::data_api::WalletRead;
+use tonic::Status;
+use web3::types::Res;
+use zcash_client_backend::data_api::{BlockSource, WalletRead};
+use zcash_client_backend::proto::compact_formats::CompactBlock;
 use zcash_client_backend::wallet::AccountId;
+use zcash_client_sqlite::error::SqliteClientError;
 use zcash_client_sqlite::wallet::init::init_accounts_table;
-use zcash_client_sqlite::{BlockDb, WalletDb};
+use zcash_client_sqlite::WalletDb;
 use zcash_primitives::consensus::NetworkUpgrade;
 use zcash_primitives::consensus::{BlockHeight, Parameters};
 use zcash_primitives::constants::mainnet as z_mainnet_constants;
@@ -25,6 +32,7 @@ mod z_coin_grpc {
     tonic::include_proto!("cash.z.wallet.sdk.rpc");
 }
 use z_coin_grpc::compact_tx_streamer_client::CompactTxStreamerClient;
+use z_coin_grpc::{BlockId, BlockRange, ChainSpec, RawTransaction};
 
 #[derive(Debug, Serialize)]
 pub struct ZSendManyItem {
@@ -164,6 +172,97 @@ impl AsRef<dyn ZRpcOps + Send + Sync> for UtxoRpcClientEnum {
     }
 }
 
+struct CompactBlockRow {
+    height: BlockHeight,
+    data: Vec<u8>,
+}
+
+/// A wrapper for the SQLite connection to the block cache database.
+pub struct BlockDb(Connection);
+
+impl BlockDb {
+    /// Opens a connection to the wallet database stored at the specified path.
+    pub fn for_path<P: AsRef<Path>>(path: P) -> Result<Self, db_common::sqlite::rusqlite::Error> {
+        Connection::open(path).map(BlockDb)
+    }
+
+    fn with_blocks<F>(
+        &self,
+        from_height: BlockHeight,
+        limit: Option<u32>,
+        mut with_row: F,
+    ) -> Result<(), SqliteClientError>
+    where
+        F: FnMut(CompactBlock) -> Result<(), SqliteClientError>,
+    {
+        // Fetch the CompactBlocks we need to scan
+        let mut stmt_blocks = self
+            .0
+            .prepare("SELECT height, data FROM compactblocks WHERE height > ? ORDER BY height ASC LIMIT ?")?;
+
+        let rows = stmt_blocks.query_map(
+            params![u32::from(from_height), limit.unwrap_or(u32::max_value()),],
+            |row| {
+                Ok(CompactBlockRow {
+                    height: BlockHeight::from_u32(row.get(0)?),
+                    data: row.get(1)?,
+                })
+            },
+        )?;
+
+        for row_result in rows {
+            let cbr = row_result?;
+            let block = CompactBlock::parse_from_bytes(&cbr.data)
+                .map_err(zcash_client_backend::data_api::error::Error::from)?;
+
+            if block.height() != cbr.height {
+                return Err(SqliteClientError::CorruptedData(format!(
+                    "Block height {} did not match row's height field value {}",
+                    block.height(),
+                    cbr.height
+                )));
+            }
+
+            with_row(block)?;
+        }
+
+        Ok(())
+    }
+
+    async fn get_latest_block(&self) -> Result<i64, MmError<db_common::sqlite::rusqlite::Error>> {
+        self.0
+            .query_row(
+                "SELECT height FROM compactblocks ORDER BY height DESC LIMIT 1",
+                db_common::sqlite::rusqlite::NO_PARAMS,
+                |row| row.get(0),
+            )
+            .map_err(MmError::new)
+    }
+
+    async fn insert_block(
+        &self,
+        height: u32,
+        cb_bytes: Vec<u8>,
+    ) -> Result<usize, MmError<db_common::sqlite::rusqlite::Error>> {
+        self.0
+            .prepare("INSERT INTO compactblocks (height, data) VALUES (?, ?)")
+            .map_err(MmError::new)?
+            .execute(params![height, cb_bytes])
+            .map_err(MmError::new)
+    }
+}
+
+impl BlockSource for BlockDb {
+    type Error = SqliteClientError;
+
+    fn with_blocks<F>(&self, from_height: BlockHeight, limit: Option<u32>, with_row: F) -> Result<(), Self::Error>
+    where
+        F: FnMut(CompactBlock) -> Result<(), Self::Error>,
+    {
+        self.with_blocks(from_height, limit, with_row)
+    }
+}
+
 pub struct ZcoinLightClient {
     grpc_client: CompactTxStreamerClient<Channel>,
     blocks_db: BlockDb,
@@ -177,6 +276,21 @@ pub enum ZcoinLightClientInitError {
     ConnectionFailure(tonic::transport::Error),
     BlocksDbInitFailure(db_common::sqlite::rusqlite::Error),
     WalletDbInitFailure(db_common::sqlite::rusqlite::Error),
+}
+
+#[derive(Debug)]
+#[non_exhaustive]
+pub enum UpdateBlocksCacheErr {
+    GrpcError(tonic::Status),
+    DbError(db_common::sqlite::rusqlite::Error),
+}
+
+impl From<tonic::Status> for UpdateBlocksCacheErr {
+    fn from(err: tonic::Status) -> Self { UpdateBlocksCacheErr::GrpcError(err) }
+}
+
+impl From<db_common::sqlite::rusqlite::Error> for UpdateBlocksCacheErr {
+    fn from(err: Error) -> Self { UpdateBlocksCacheErr::DbError(err) }
 }
 
 impl ZcoinLightClient {
@@ -214,6 +328,46 @@ impl ZcoinLightClient {
             blocks_db,
             wallet_db,
         })
+    }
+
+    pub async fn get_latest_block(&mut self) -> Result<BlockId, MmError<tonic::Status>> {
+        let request = tonic::Request::new(ChainSpec {});
+        self.grpc_client
+            .get_latest_block(request)
+            .await
+            .map(|res| res.into_inner())
+            .map_err(MmError::new)
+    }
+
+    pub async fn update_blocks_cache(&mut self) -> Result<u64, MmError<UpdateBlocksCacheErr>> {
+        let current_blockchain_block = self.get_latest_block().await?;
+        let current_block_in_db = self.blocks_db.get_latest_block().await?;
+
+        let from_block = current_block_in_db as u64 + 1;
+        let current_block: u64 = current_blockchain_block.height;
+
+        if current_block >= from_block {
+            let request = tonic::Request::new(BlockRange {
+                start: Some(BlockId {
+                    height: from_block,
+                    hash: Vec::new(),
+                }),
+                end: Some(BlockId {
+                    height: current_block,
+                    hash: Vec::new(),
+                }),
+            });
+
+            let mut response = self.grpc_client.get_block_range(request).await?;
+
+            while let Ok(Some(block)) = response.get_mut().message().await {
+                println!("Got block {:?}", block);
+                self.blocks_db
+                    .insert_block(block.height as u32, block.encode_to_vec())
+                    .await?;
+            }
+        }
+        Ok(current_block)
     }
 }
 
@@ -269,22 +423,6 @@ fn try_grpc() {
         Ok(())
     }
 
-    pub fn get_latest_block(db_cache: &Connection) -> Result<i64, rusqlite::Error> {
-        db_cache.query_row(
-            "SELECT height FROM compactblocks ORDER BY height DESC LIMIT 1",
-            NO_PARAMS,
-            |row| row.get(0),
-        )
-    }
-
-    fn insert_into_cache(db_cache: &Connection, height: u32, cb_bytes: Vec<u8>) {
-        db_cache
-            .prepare("INSERT INTO compactblocks (height, data) VALUES (?, ?)")
-            .unwrap()
-            .execute(db_common::sqlite::rusqlite::params![height, cb_bytes])
-            .unwrap();
-    }
-
     let z_key = decode_extended_spending_key(z_mainnet_constants::HRP_SAPLING_EXTENDED_SPENDING_KEY, "secret-extended-key-main1q0k2ga2cqqqqpq8m8j6yl0say83cagrqp53zqz54w38ezs8ly9ly5ptamqwfpq85u87w0df4k8t2lwyde3n9v0gcr69nu4ryv60t0kfcsvkr8h83skwqex2nf0vr32794fmzk89cpmjptzc22lgu5wfhhp8lgf3f5vn2l3sge0udvxnm95k6dtxj2jwlfyccnum7nz297ecyhmd5ph526pxndww0rqq0qly84l635mec0x4yedf95hzn6kcgq8yxts26k98j9g32kjc8y83fe").unwrap().unwrap();
 
     let uri = Uri::from_str("http://zombie.sirseven.me:443").unwrap();
@@ -295,47 +433,17 @@ fn try_grpc() {
     ))
     .unwrap();
 
-    let request = tonic::Request::new(ChainSpec {});
-    let latest_block_response = block_on(client.grpc_client.get_latest_block(request)).unwrap();
-    let latest_block_height = latest_block_response.into_inner().height;
+    let latest_block = block_on(client.get_latest_block()).unwrap();
+    let latest_block_height = latest_block.height;
+    let current_block = block_on(client.update_blocks_cache()).unwrap();
 
-    let cache_file = "test_cache_zombie.db";
-    let cache_sql = Connection::open(cache_file).unwrap();
-    init_cache_database(&cache_sql).unwrap();
-
-    let from_block = get_latest_block(&cache_sql).unwrap() as u64 + 1;
-    let current_block: u64 = latest_block_height;
-
-    if current_block >= from_block {
-        let request = tonic::Request::new(BlockRange {
-            start: Some(BlockId {
-                height: from_block,
-                hash: Vec::new(),
-            }),
-            end: Some(BlockId {
-                height: current_block,
-                hash: Vec::new(),
-            }),
-        });
-
-        let mut response = block_on(client.grpc_client.get_block_range(request)).unwrap();
-
-        while let Ok(Some(block)) = block_on(response.get_mut().message()) {
-            println!("Got block {:?}", block);
-            insert_into_cache(&cache_sql, block.height as u32, block.encode_to_vec());
-        }
-    }
-
-    drop(cache_sql);
+    // init_cache_database(&cache_sql).unwrap();
 
     let network = ZombieNetwork {};
-    let db_cache = BlockDb::for_path(cache_file).unwrap();
-    let db_file = "test_wallet_zombie.db";
-    let db_read = WalletDb::for_path(db_file, network).unwrap();
-    init_wallet_db(&db_read).unwrap();
+    init_wallet_db(&client.wallet_db).unwrap();
 
     // init_accounts_table(&db_read, &[(&z_key).into()]).unwrap();
-    let mut db_data = db_read.get_update_ops().unwrap();
+    let mut db_data = client.wallet_db.get_update_ops().unwrap();
 
     // 1) Download new CompactBlocks into db_cache.
 
@@ -343,7 +451,7 @@ fn try_grpc() {
     //
     // Given that we assume the server always gives us correct-at-the-time blocks, any
     // errors are in the blocks we have previously cached or scanned.
-    if let Err(e) = validate_chain(&network, &db_cache, db_data.get_max_height_hash().unwrap()) {
+    if let Err(e) = validate_chain(&network, &client.blocks_db, db_data.get_max_height_hash().unwrap()) {
         match e {
             SqliteClientError::BackendError(Error::InvalidChain(lower_bound, _)) => {
                 // a) Pick a height to rewind to.
@@ -377,15 +485,17 @@ fn try_grpc() {
     // At this point, the cache and scanned data are locally consistent (though not
     // necessarily consistent with the latest chain tip - this would be discovered the
     // next time this codepath is executed after new blocks are received).
-    scan_cached_blocks(&network, &db_cache, &mut db_data, None).unwrap();
+    scan_cached_blocks(&network, &client.blocks_db, &mut db_data, None).unwrap();
 
-    let balance = db_read
+    let balance = client
+        .wallet_db
         .get_balance_at(AccountId(0), BlockHeight::from_u32(current_block as u32))
         .unwrap();
 
     println!("balance {:?}", balance);
 
-    let notes = db_read
+    let notes = client
+        .wallet_db
         .get_spendable_notes(AccountId(0), BlockHeight::from_u32(current_block as u32))
         .unwrap();
 
