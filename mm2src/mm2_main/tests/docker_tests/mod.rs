62d9cce386beaa7f9fc834e219328a18ce4d4392
rozhkovdmitrii <rozhkovdmitrii@infatica.io>
2023-04-20 17:50:43 +0500

    Revert "Got rid of fn dummy" This reverts commit 65b088df8259e84e4cf2012d81924e58c6caf8d7.

--- mm2src/mm2_main/tests/docker_tests/mod.rs
+++ mm2src/mm2_main/tests/docker_tests/mod.rs
@@ -9,3 +9,8 @@ mod swaps_confs_settings_sync_tests;
 mod swaps_file_lock_tests;
 
 #[cfg(feature = "enable-solana")] mod solana_tests;
+
+// dummy test helping IDE to recognize this as test module
+#[test]
+#[allow(clippy::assertions_on_constants)]
+fn dummy() { assert!(true) }
